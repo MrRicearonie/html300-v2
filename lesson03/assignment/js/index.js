bb62d4ec30df84f42c4ef60abcbe8c36cbd7533d
Christopher Rempe <43194904+MrRicearonie@users.noreply.github.com>
2022-05-18 14:25:11 -0700

    Programming languages have been added

--- lesson03/assignment/js/index.js
+++ lesson03/assignment/js/index.js
@@ -7,6 +7,10 @@ fetch('./data/data.json')
     .then(data => {
         // For each entry, create the card and push it to the html file
         data.forEach(el => {
+            // Get the languages known and make them into a string
+            const languages = el.codeLanguages.join(', ')
+
+            // Create the card element, and add the HTML to it
             const card = document.createElement('div')
             card.setAttribute('class', 'card-outline')
             card.innerHTML = `
@@ -22,12 +26,14 @@ fetch('./data/data.json')
                         <p><strong>School:</strong> ${el.school}</p>
                         <p><strong>Major:</strong> ${el.major}</p>
                         <p><strong>Email:</strong> <a href = "mailto: ${el.email}">${el.email}</a></p>
+                        <p class="languages"><strong>Languages:</strong> ${languages}</p>
                         <div class="linkedin">
                             <img src="./img/linkedin.svg" alt="LinkedIn"> <p><a href="https://${el.linkedInUrl}">${el.linkedInUrl}</a></p>
                         </div>
                     </div>
                 </div>
             `
+            // Add the card to the page
             container.append(card)
         });
     })
