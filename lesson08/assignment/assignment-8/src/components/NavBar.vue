bf7272bda22c4c04948e78588c6d89ed0ac4db09
Christopher Rempe <43194904+MrRicearonie@users.noreply.github.com>
2022-07-11 13:34:54 -0700

    Added separate component files for images/posts

---
+++ lesson08/assignment/assignment-8/src/components/NavBar.vue
@@ -0,0 +1,18 @@
+<template>
+    <!-- Set up the navbar with the primary color -->
+    <b-navbar toggleable="sm" type="dark" variant="primary">
+        <!-- To keep the buttons in a good position, wrap them in a container -->
+        <b-container>
+            <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+            <b-collapse id="nav-collapse" is-nav>
+                <!-- Add the router links -->
+                <b-navbar-nav>
+                    <router-link to="/home" class="nav-link">Home</router-link>
+                    <router-link to="/personal" class="nav-link">Personal</router-link>
+                    <router-link to="/shop" class="nav-link">Shop</router-link>
+                    <router-link to="/blog" class="nav-link">Blog</router-link>
+                </b-navbar-nav>
+            </b-collapse>
+        </b-container>
+    </b-navbar>
+</template>
---
+++ lesson08/assignment/assignment-8/src/components/ShopContent.vue
@@ -0,0 +1,171 @@
+<!-- Used to be ShopContent -->
+<template>
+    <div>
+        <div class="text-center my-5">
+            <h1 class="display-4 text-secondary">{{ title }}</h1>
+        </div>
+
+        <!-- Create a carousel for the images -->
+        <b-carousel
+            id="carousel-fade"
+            fade
+            controls
+        >
+            <!-- Go through the images and add them to the carousel -->
+            <b-carousel-slide v-for="image in carouselImages" :key="image.id" :img-src="image.src" :img-alt="image.alt">
+            </b-carousel-slide>
+        </b-carousel>
+
+        <b-container>
+            <b-row>
+                <!-- For each item in items, set up the shopping card from the Shopping Cards file -->
+                <ShoppingCard
+                    v-for="(itemList, index) in items"
+                    :key="index"
+                    :itemList="itemList"
+                    :img_src="itemList.img_src"
+                    :img_alt="itemList.img_alt"
+                    :title="itemList.title"
+                    :desc="itemList.desc"
+                    :btn_text="itemList.btn_text"
+                />
+            </b-row>
+        </b-container>
+    </div>
+</template>
+
+<script>
+    import ShoppingCard from "../components/ShoppingCards";
+
+    export default {
+        components: { ShoppingCard },
+        data: () => {
+            return {
+                title: 'Responsive Images',
+                carouselImages: [
+                    {
+                        title: 'Basement',
+                        src: require('@/assets/TheBasementTest.jpg'),
+                        alt: "A basement of an abandoned building covered in graffiti"
+                    },
+                    {
+                        title: 'Beach',
+                        src: require('@/assets/RubyBeachTest.jpg'),
+                        alt: "Sunset over the ocean from a beach with a tall island"
+                    },
+                    {
+                        title: 'Moss',
+                        src: require('@/assets/MossRockGrassTest.jpg'),
+                        alt: "A rock covered in moss"
+                    },
+                ],
+
+                // Each item has a photo, title, description, and two buttons
+                items: [
+                    {
+                        img_src: require('@/assets/RockPile.jpg'),
+                        img_alt: "test test",
+                        title: "testing",
+                        desc: "This is a test",
+                        btn_text_buy: "Buy",
+                        btn_text_more: "Border"
+                    },
+                    {
+                        img_src: require('@/assets/RockPile.jpg'),
+                        img_alt: "test test",
+                        title: "testing",
+                        desc: "This is a test",
+                        btn_text_buy: "Buy",
+                        btn_text_more: "Border"
+                    },
+                    {
+                        img_src: require('@/assets/RockPile.jpg'),
+                        img_alt: "test test",
+                        title: "testing",
+                        desc: "This is a test",
+                        btn_text_buy: "Buy",
+                        btn_text_more: "Border"
+                    },
+                    {
+                        img_src: require('@/assets/RockPile.jpg'),
+                        img_alt: "test test",
+                        title: "testing",
+                        desc: "This is a test",
+                        btn_text_buy: "Buy",
+                        btn_text_more: "Border"
+                    },
+                    {
+                        img_src: require('@/assets/RockPile.jpg'),
+                        img_alt: "test test",
+                        title: "testing",
+                        desc: "This is a test",
+                        btn_text_buy: "Buy",
+                        btn_text_more: "Border"
+                    },
+                    {
+                        img_src: require('@/assets/RockPile.jpg'),
+                        img_alt: "test test",
+                        title: "testing",
+                        desc: "This is a test",
+                        btn_text_buy: "Buy",
+                        btn_text_more: "Border"
+                    },
+                    {
+                        img_src: require('@/assets/RockPile.jpg'),
+                        img_alt: "test test",
+                        title: "testing",
+                        desc: "This is a test",
+                        btn_text_buy: "Buy",
+                        btn_text_more: "Border"
+                    },
+                    {
+                        img_src: require('@/assets/RockPile.jpg'),
+                        img_alt: "test test",
+                        title: "testing",
+                        desc: "This is a test",
+                        btn_text_buy: "Buy",
+                        btn_text_more: "Border"
+                    },
+                    {
+                        img_src: require('@/assets/RockPile.jpg'),
+                        img_alt: "test test",
+                        title: "testing",
+                        desc: "This is a test",
+                        btn_text_buy: "Buy",
+                        btn_text_more: "Border"
+                    },
+                    {
+                        img_src: require('@/assets/RockPile.jpg'),
+                        img_alt: "test test",
+                        title: "testing",
+                        desc: "This is a test",
+                        btn_text_buy: "Buy",
+                        btn_text_more: "Border"
+                    },
+                    {
+                        img_src: require('@/assets/RockPile.jpg'),
+                        img_alt: "test test",
+                        title: "testing",
+                        desc: "This is a test",
+                        btn_text_buy: "Buy",
+                        btn_text_more: "Border"
+                    },
+                    {
+                        img_src: require('@/assets/RockPile.jpg'),
+                        img_alt: "test test",
+                        title: "testing",
+                        desc: "This is a test",
+                        btn_text_buy: "Buy",
+                        btn_text_more: "Border"
+                    },
+                ]
+            };
+        },
+    };
+</script>
+
+<style>
+    .carousel {
+        padding-bottom: 3rem;
+    }
+</style>
